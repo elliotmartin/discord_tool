017d48d83be7012333089d280b8bacc73a6419e9
elliotmartin <ejmart14@stlawu.edu>
2020-01-07 19:33:22 -0800

    Update output loop

--- boards_users.py
+++ boards_users.py
@@ -3,82 +3,37 @@ import json
 def match_users(filename, tags):
     with open(filename, 'r') as f:
         data = json.load(f)
+        print(data)
     with open(tags, 'r') as f:
-        tags = f.readlines()
-        tags = [t[0:t.find('#')].lower().strip() for t in tags]
-
+        tags = json.load(f)
+        print(tags)
+        just_tags = [t[0:t.find('#')].lower().strip() for t in tags.keys()]
+#the tags list needs to be declared as a seperate variable so that we can still index the JSON tags to find discord users from battletags, other variables need to be updated appropriately
     matches = []
     
     #woooo triple loop. new highscore! I don't think this is too horrific? 
     for country in data.keys():
         for mode in data[country].keys():
             for k in data[country][mode].keys():
-                if k.lower().strip() in tags:
+                if k.lower().strip() in just_tags:
                 #name mode country rank
-                    matches.append((mode,  country,  k, str(data[country][mode][k])))
+                    player_name = str(data[country][mode][k])
+                    matches.append((mode,  country,  k, player_name, ))#tags[player_name]))
                 
     
     return matches
 
 prev_season = "74"
-matched = match_users('./json/' + prev_season  + '.json', './json/tags.txt')
+matched = match_users('./json/' + prev_season  + '.json', './json/tags.json')
 
 print("Congratulations to the following users from our server who placed on the Official Hearthstone leaderboards last month!")
 
-print("**Standard**")
-print("__US__:")
-for m in matched:
-    if (m[0] == "STD") and (m[1] == "US"):
-        print(m[2] + " placed " + m[3])
 
-print('\n')
-print("__EU__:")
-for m in matched:
-    if (m[0] == "STD") and (m[1] == "EU"):
-        print(m[2] + " placed " + m[3])
+for mode in ['STD', 'WLD', 'BG']:
+    print('**' + mode + '**')
+    for region in ['US', 'EU', 'APAC']:
+        print('__' + region + '__')
+        for m in matched:
+            if (m[0] == mode) and (m[1] == region):
+                print(m[2] + " placed " + m[3])
 
-print('\n')
-print("__APAC__:")
-for m in matched:
-    if (m[0] == "STD") and (m[1] == "AP"):
-        print(m[2] + " placed " + m[3])
-
-print('\n')
-
-print("**Wild**")
-print("__US__:")
-for m in matched:
-    if (m[0] == "WLD") and (m[1] == "US"):
-        print(m[2] + " placed " + m[3])
-
-print('\n')
-print("__EU__:")
-for m in matched:
-    if (m[0] == "WLD") and (m[1] == "EU"):
-        print(m[2] + " placed " + m[3])
-
-print('\n')
-print("__APAC__:")
-for m in matched:
-    if (m[0] == "WLD") and (m[1] == "AP"):
-        print(m[2] + " placed " + m[3])
-
-print('\n')
-
-print("**Battlegrounds**")
-print("__US__:")
-for m in matched:
-    if (m[0] == "BG") and (m[1] == "US"):
-        print(m[2] + " placed " + m[3])
-
-print('\n')
-print("__EU__:")
-for m in matched:
-    if (m[0] == "BG") and (m[1] == "EU"):
-        print(m[2] + " placed " + m[3])
-
-print('\n')
-print("__APAC__:")
-for m in matched:
-    if (m[0] == "BG") and (m[1] == "AP"):
-        print(m[2] + " placed " + m[3])
--- discord_bot.py
+++ discord_bot.py
@@ -2,6 +2,7 @@ import os
 import discord
 from dotenv import load_dotenv
 import re
+import json
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,14 +19,18 @@ async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
     channel = discord.utils.find(lambda c: c.name == "battletags", guild.channels)
     print(channel)
-    tags = []
-    async for message in channel.history(limit = 25*34):
+    tags = {}
+    async for message in channel.history(limit = 25*44):
         tag = re.findall(pattern, message.content)
-        tags.append(tag)
-    tags = flatten(tags)
-    with open("./json/tags.txt", "w") as f:
-        for t in tags:
-            f.write(t + "\n")
+        author = message.author.name
+        if tag:
+            tags[tag[0]] = author
+    #tags = flatten(tags)
+    tags['gallon'] = 'gallon'
+    tags['jambre'] = 'jambre'
+    with open("./json/tags.json", "w") as f:
+        json.dump(tags, f)
+
     print("done")
 
 client.run(TOKEN)
